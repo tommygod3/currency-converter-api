147c2603bc1e3f0c0c39e5004e97e5c683f5dc66
tommygod3 <tommygod1@live.com>
2020-01-12 20:50:28 +0000

    New data only fetched when updated needed

--- currency_converter_api/currency/views.py
+++ currency_converter_api/currency/views.py
@@ -13,8 +13,8 @@ class CurrencyViewSet(viewsets.ModelViewSet):
     serializer_class = CurrencySerializer
 
     api_url = "http://apilayer.net/api/live?access_key=2938f0796b94b63bdd3d56e0faa2628e"
-    
-    def list(self, request, format=None):
+
+    def update_all_currencies(self):
         content = requests.get(self.api_url).content
         api_dict = json.loads(content)
 
@@ -23,24 +23,28 @@ class CurrencyViewSet(viewsets.ModelViewSet):
                 usd_gbp_rate = usd_rate
 
         for symbol, usd_rate in api_dict["quotes"].items():
-            print(symbol[3:], usd_rate)
             data = {
                 "gbp_rate": usd_rate/usd_gbp_rate,
-                "last_updated": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+                "last_updated": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
             }
 
             try:
                 obj = Currency.objects.get(symbol=symbol[3:])
-                if datetime.now(timezone.utc) - obj.last_updated > timedelta(days=7):
-                    for key, value in data.items():
-                        setattr(obj, key, value)
-                    obj.save()
+                for key, value in data.items():
+                    setattr(obj, key, value)
+                obj.save()
 
             except Currency.DoesNotExist:
                 new_values = {"symbol": symbol[3:]}
                 new_values.update(data)
                 obj = Currency(**new_values)
                 obj.save()
+    
+    def list(self, request, format=None):
+        for currency in self.queryset:
+            if datetime.now(timezone.utc) - currency.last_updated > timedelta(days=7):
+                self.update_all_currencies()
+                break
 
         serializer = self.serializer_class(self.queryset, many=True)
         return Response(serializer.data)
@@ -49,7 +53,6 @@ class CurrencyViewSet(viewsets.ModelViewSet):
         currency = get_object_or_404(self.queryset, pk=pk)
 
         if datetime.now(timezone.utc) - currency.last_updated > timedelta(days=1):
-            print("Change")
             content = requests.get(self.api_url+"&currencies=GBP").content
             gbp_dict = json.loads(content)
             usd_gbp_rate = gbp_dict["quotes"]["USDGBP"]
@@ -62,7 +65,7 @@ class CurrencyViewSet(viewsets.ModelViewSet):
             gbp_rate = api_dict["quotes"][f"USD{pk}"] / usd_gbp_rate
 
             currency.gbp_rate = gbp_rate
-            currency.last_updated = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+            currency.last_updated = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
             currency.save()
   
 
