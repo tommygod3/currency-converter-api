23095247ed6b0f7f220b9aa2b2f37d3314873cd7
tommygod3 <tommygod1@live.com>
2020-01-12 20:53:01 +0000

    Removed need for name

--- currency_converter_api/currency/models.py
+++ currency_converter_api/currency/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 class Currency(models.Model):
     symbol = models.CharField(max_length=3, primary_key=True)
-    #name = models.CharField(max_length=100, primary_key=True)
     gbp_rate = models.FloatField()
     last_updated = models.DateTimeField()
