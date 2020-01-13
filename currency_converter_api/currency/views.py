54cd1c538d0c7a907d0d074f817e43a86ae9b4ad
tommygod3 <tommygod1@live.com>
2020-01-13 16:12:03 +0000

    Added excption handling for API down

--- currency_converter_api/currency/views.py
+++ currency_converter_api/currency/views.py
@@ -17,31 +17,34 @@ class CurrencyViewSet(viewsets.ModelViewSet):
     api_url = "http://apilayer.net/api/live?access_key=2938f0796b94b63bdd3d56e0faa2628e"
 
     def update_all_currencies(self):
-        content = requests.get(self.api_url).content
-        api_dict = json.loads(content)
+        try:
+            content = requests.get(self.api_url).content
+            api_dict = json.loads(content)
 
-        for symbol, usd_rate in api_dict["quotes"].items():
-            if symbol == "USDGBP":
-                usd_gbp_rate = usd_rate
+            for symbol, usd_rate in api_dict["quotes"].items():
+                if symbol == "USDGBP":
+                    usd_gbp_rate = usd_rate
 
-        for symbol, usd_rate in api_dict["quotes"].items():
-            data = {
-                "rate": usd_rate/usd_gbp_rate,
-                "name": currency_names.get(symbol[3:], "None"),
-                "last_updated": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
-            }
+            for symbol, usd_rate in api_dict["quotes"].items():
+                data = {
+                    "rate": usd_rate/usd_gbp_rate,
+                    "name": currency_names.get(symbol[3:], "None"),
+                    "last_updated": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
+                }
 
-            try:
-                obj = Currency.objects.get(symbol=symbol[3:])
-                for key, value in data.items():
-                    setattr(obj, key, value)
-                obj.save()
+                try:
+                    obj = Currency.objects.get(symbol=symbol[3:])
+                    for key, value in data.items():
+                        setattr(obj, key, value)
+                    obj.save()
 
-            except Currency.DoesNotExist:
-                new_values = {"symbol": symbol[3:]}
-                new_values.update(data)
-                obj = Currency(**new_values)
-                obj.save()
+                except Currency.DoesNotExist:
+                    new_values = {"symbol": symbol[3:]}
+                    new_values.update(data)
+                    obj = Currency(**new_values)
+                    obj.save()
+        except Exception:
+            pass
 
     def list(self, request, format=None):
         if (request.GET.get("update", None) == "True"):
@@ -73,21 +76,24 @@ class CurrencyViewSet(viewsets.ModelViewSet):
         currency = get_object_or_404(self.queryset, pk=pk)
 
         if datetime.now(timezone.utc) - currency.last_updated > timedelta(days=1):
-            content = requests.get(self.api_url+"&currencies=GBP").content
-            gbp_dict = json.loads(content)
-            usd_gbp_rate = gbp_dict["quotes"]["USDGBP"]
+            try:
+                content = requests.get(self.api_url+"&currencies=GBP").content
+                gbp_dict = json.loads(content)
+                usd_gbp_rate = gbp_dict["quotes"]["USDGBP"]
 
-            params = f"&currencies={pk}"
+                params = f"&currencies={pk}"
 
-            content = requests.get(self.api_url+params).content
-            api_dict = json.loads(content)
+                content = requests.get(self.api_url+params).content
+                api_dict = json.loads(content)
 
-            gbp_rate = api_dict["quotes"][f"USD{pk}"] / usd_gbp_rate
+                gbp_rate = api_dict["quotes"][f"USD{pk}"] / usd_gbp_rate
 
-            currency.name = currency_names.get(pk, "None"),
-            currency.rate = gbp_rate
-            currency.last_updated = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
-            currency.save()
+                currency.name = currency_names.get(pk, "None"),
+                currency.rate = gbp_rate
+                currency.last_updated = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
+                currency.save()
+            except Exception:
+                pass
   
 
         serializer = self.serializer_class(currency)
@@ -103,4 +109,3 @@ class CurrencyViewSet(viewsets.ModelViewSet):
             enriched_data = copy.deepcopy(serializer.data)
             enriched_data["source"] = "GBP"
             return Response(enriched_data)
-            
